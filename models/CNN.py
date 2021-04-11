cabbc69055f72a074c47f7cae4637cc966f88bf3
Eniko Toth <enikodonatellatoth@gmail.com>
2021-04-11 21:05:04 +0200

    Create more CNNs

--- models/CNN.py
+++ models/CNN.py
@@ -32,6 +32,67 @@ class LeNet5(nn.Module):
         return logits, probs
 
 
+class LeNet5DecKernelSize(nn.Module):
+    def __init__(self, n_classes=10):
+        super(LeNet5DecKernelSize, self).__init__()
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=11, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=6, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=2, stride=1),
+            nn.Tanh()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=120, out_features=84),
+            nn.Tanh(),
+            nn.Linear(in_features=84, out_features=n_classes),
+        )
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, 1)
+        logits = self.classifier(x)
+        probs = F.softmax(logits, dim=1)
+        return logits, probs
+
+
+class LeNet5SmallKernels(nn.Module):
+    def __init__(self, n_classes=10):
+        super(LeNet5SmallKernels, self).__init__()
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=2, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=26, kernel_size=3, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=26, out_channels=120, kernel_size=2, stride=1),
+            nn.Tanh()
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=120, out_features=84),
+            nn.Tanh(),
+            nn.Linear(in_features=84, out_features=n_classes),
+        )
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, 1)
+        logits = self.classifier(x)
+        probs = F.softmax(logits, dim=1)
+        return logits, probs
+
+
 class LeNet5MaxPool(nn.Module):
     def __init__(self, n_classes=10):
         super(LeNet5MaxPool, self).__init__()
--- models/convert_model_to_onnx.py
+++ models/convert_model_to_onnx.py
@@ -1,14 +1,17 @@
 import torch
 import sys
 
-from CNN import LeNet5
+from CNN import *
+
+model = LeNet5() # check CNN.py for more models
+name = "LeNet5"
 
 device = torch.device('cpu')
 print("Loading model")
-pytorch_model = LeNet5()
+pytorch_model = model
 print("Loading weight data")
-pytorch_model.load_state_dict(torch.load('saved/LeNet5', map_location=device))
+pytorch_model.load_state_dict(torch.load(f'saved/{name}', map_location=device))
 pytorch_model.eval()
 dummy_input = torch.zeros(size=(1,1, 28, 28))
 print("Exporting model")
-torch.onnx.export(pytorch_model, dummy_input, '../MNISTtester/onnx/onnx_model.onnx', verbose=True)
+torch.onnx.export(pytorch_model, dummy_input, f'../docs/onnx/{name}.onnx', verbose=True)
